669e8b952bcf4690d267bf9c158123653e4e295a
keklol5050 <kek.lol5050@gmail.com>
2024-02-23 23:34:20 +0200

    first

--- src/main/java/com/crypto/analysis/main/vo/DataObject.java
+++ src/main/java/com/crypto/analysis/main/vo/DataObject.java
@@ -1,4 +1,17 @@
 package com.crypto.analysis.main.vo;
 
+import com.binance.connector.futures.client.impl.UMFuturesClientImpl;
+import java.util.LinkedHashMap;
+
 public class DataObject {
+    public static void main(String[] args) {
+        UMFuturesClientImpl client = new UMFuturesClientImpl();
+        LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
+        parameters.put("symbol", "BTCUSDT");
+        parameters.put("interval", "15m");
+        parameters.put("limit", "60");
+        String result = client.market().klines(parameters);
+        System.out.println(result);
+
+    }
 }
