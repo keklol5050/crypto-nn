bca0c9cca3ff11378683a9fe606c0d57a149506c
Андрій <drebytert@gmail.com>
2024-02-27 00:20:18 +0200

    а2

--- src/main/java/com/crypto/analysis/main/TelegramBot.java
+++ src/main/java/com/crypto/analysis/main/TelegramBot.java
@@ -1,20 +1,58 @@
 package com.crypto.analysis.main;
 
 
-import lombok.Data;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@Data
-@PropertySource("application.properties")
-class TelegramBot{
-    @Value("${Botname}")
-    String BotName;
-    @Value("${Botkey}")
-    String BotKey;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+class TelegramBot extends TelegramLongPollingBot {
+
+    final ValuesBot valuesBot;
+    public TelegramBot(ValuesBot valuesBot) {
+        this.valuesBot = valuesBot;
+    }
+    @Override
+    public String getBotUsername() {
+        return valuesBot.getBotName();
+    }
+    @Override
+    public String getBotToken() {
+        return valuesBot.getBotKey();
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+          if(update.getMessage().hasText()) {
+              String TextMessage = update.getMessage().getText();
+              long chatId = update.getMessage().getChatId();
+              switch(TextMessage){
+                  case "/start":
+                      startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                default:sendMessage(chatId,"Suck suck");
+
+              }
+          }
+
+    }
+    private void startCommandReceived(long ChatId,String name){
+     String answer = "Hi" + name + "Let's start the working";
+     sendMessage(ChatId,answer);
+    }
+    private void sendMessage(long chatId,String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+        try{
+            execute(sendMessage);
+        }catch (TelegramApiException exception){
+
+        }
+    }
 }
 
 
