26ed97fc4f72f3a5158184677364add5427aa773
Андрій <drebytert@gmail.com>
2024-02-26 22:33:48 +0200

    а1

--- src/main/java/com/crypto/analysis/main/MainBot.java
+++ src/main/java/com/crypto/analysis/main/MainBot.java
@@ -1,12 +1,3 @@
 package com.crypto.analysis.main;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class MainBot {
-    public static void main(String[] args) throws TelegramApiException {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(new TelegramBot());
-    }
-}
--- src/main/java/com/crypto/analysis/main/ValuesBot.java
+++ src/main/java/com/crypto/analysis/main/ValuesBot.java
@@ -1,20 +1,19 @@
 package com.crypto.analysis.main;
 
-
 import lombok.Data;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@Data
-@PropertySource("application.properties")
-class TelegramBot{
-    @Value("${Botname}")
-    String BotName;
-    @Value("${Botkey}")
-    String BotKey;
-}
 
+
+    @Configuration
+    @Data
+    @PropertySource("application.properties")
+    public class ValuesBot {
+        @Value("${Botname}")
+        String BotName;
+        @Value("${Botkey}")
+        String BotKey;
+    }
 
